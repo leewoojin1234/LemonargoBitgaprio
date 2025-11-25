3b36c721cf3937a5302e90d53a3abacd4c82f9c5
leewoojin1234 <geonui542@gmail.com>
2025-11-25 12:21:02 +0900

    feat :: Initial

---
+++ src/main/java/com/dgsw/lemon_debt_slayer/LemonDebtSlayerApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.lemon_debt_slayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonDebtSlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemonDebtSlayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/lemon_debt_slayer/LemonDebtSlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.lemon_debt_slayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemonDebtSlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
