d56f75dcc596067b234eaaa255185d3fc1e3e05a
leewoojin1234 <geonui542@gmail.com>
2025-11-25 20:02:05 +0900

    Spring Scheduling 추가

--- src/main/java/com/dgsw/lemon_debt_slayer/LemonDebtSlayerApplication.java
+++ src/main/java/com/dgsw/lemon_debt_slayer/LemonDebtSlayerApplication.java
@@ -3,7 +3,9 @@ package com.dgsw.lemon_debt_slayer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class LemonDebtSlayerApplication {
